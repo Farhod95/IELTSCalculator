c6c0836b5626e6514c114e99a53b339f81a4a09e
Farhod <farhodbekisomiddinov5@gmail.com>
2026-01-17 20:00:10 +0500

    added

--- IELTSCalculator/Program.cs
+++ IELTSCalculator/Program.cs
@@ -5,7 +5,7 @@
         private static void Main(string[] args)
         {
             Console.WriteLine("==============================================================");
-            Console.WriteLine("        IELTS BALLARINI HISOBLASH KALKULYATORI !");
+            Console.WriteLine("        IELTS BALLARINI HISOBLASH KALKULYATORI ");
             Console.WriteLine("==============================================================\n");
 
             // ================= LISTENING =================
